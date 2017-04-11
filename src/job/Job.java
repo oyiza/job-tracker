0a6635ed270c7c85007aa5a3dd562c03368010c5
oyiza <epraise17@yahoo.com>
2017-04-10 22:54:59 -0400

    Array will not work for JobContainer. List or ArrayList?

--- src/job/Job.java
+++ src/job/Job.java
@@ -1,10 +1,81 @@
 package job;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Job {
+	
+	public String title, companyName;
+	public String date;
+	public String status;
+	
+	//record the date and time the job class was created and store as string
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	Date jobDate = new Date();
+	
+	/**
+	 * Constructor for class 'Job'
+	 * @param position: name of job title
+	 * @param company: name of company
+	 */
+	public Job (String position, String company) {
+		this.title = position;
+		this.companyName = company;
+		//this.date = dateFormat.format(date);
+		this.date = dateFormat.format(jobDate);
+		this.status = "Awaiting reply...";
+	}
+	
+	/** @return current job title. */
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setTitle(String newTitle) {
+		this.title = newTitle;
+	}
+	
+	/** @return current company name. */
+	public String getCompanyName() {
+		return this.companyName;
+	}
+	
+	public void setCompanyName(String newCompanyName) {
+		this.companyName = newCompanyName;
+	}
+	
+	/** @return the exact date and time job class was created. */
+	public String getDate() {
+		return this.date;
+		// TODO this date needs to be initialized only once and
+		// remain unchanged. Might need to use Timestamp
+	}
+	
+	public void setStatus(String newStatus) {
+		this.status = newStatus;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	/** @return string representation of Job details. */
+	public String toString() {
+		String result = "\n";
+		result = "Job title: " + this.title + "\nCompany Applied to: " +
+				this.companyName + "\nStatus: " + this.status + "\nDate applied: "
+				+ this.date;
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//testing that the time works
+		Job x = new Job("Software Developer", "IBM");
+		System.out.println("testing date: " + x.getDate());
+		System.out.println("\n" + x.toString());
 	}
 
 }
---
+++ src/job/JobContainer.java
@@ -0,0 +1,20 @@
+package job;
+
+public class JobContainer {
+	
+	//Array to store the jobs
+	Job[] jobs;
+	
+	public JobContainer(int size) {
+		this.jobs = new Job[size];
+	}
+	
+	public void addJob(Job newJob) {
+		// TODO
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
